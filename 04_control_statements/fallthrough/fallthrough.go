bd2b8d7948f1fe3992fc058e5798001997fe60c0
krishnatandon1208 <krishnatandon.1208@gmail.com>
2020-05-02 04:51:04 +0530

    without fallthrough

---
+++ 04_control_statements/fallthrough/fallthrough.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	num := 9
+
+	switch {
+	case num != 10:
+		fmt.Printf("%d is not equal to 10", num)
+	case num < 10:
+		fmt.Printf("%d is less than 10", num)
+	case num > 10:
+		fmt.Printf("%d is greater than 10", num)
+	} //switch
+}
