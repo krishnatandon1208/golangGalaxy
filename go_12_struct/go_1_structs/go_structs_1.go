94efc43de500444ddcfa0b85b99195e2d9cf2c9c
krishnatandon1208 <krishnatandon.1208@gmail.com>
2020-06-09 07:04:59 +0530

    Accessing attributes of Struct

--- go_12_struct/go_1_structs/go_structs_1.go
+++ go_12_struct/go_1_structs/go_structs_1.go
@@ -7,18 +7,19 @@ import "fmt"
 //User := is of type struct
 type User struct {
 	ID        int
-	firstName string
-	lastName  string
-	email     string
+	FirstName string
+	LastName  string
+	Email     string
 }
 
 func main() {
 	u := User{
 		ID:        1,
-		firstName: "Krishna",
-		lastName:  "Tandon",
-		email:     "krishnatandon.1208@gmail.com",
+		FirstName: "Krishna",
+		LastName:  "Tandon",
+		Email:     "krishnatandon.1208@gmail.com",
 	}
 
 	fmt.Println(u)
+	fmt.Printf("First Name : %s", u.FirstName)
 }
