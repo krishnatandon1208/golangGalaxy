b2bd6a904246ced65c20c8e5a10387fa11d08dd9
krishnatandon1208 <krishnatandon.1208@gmail.com>
2020-06-09 06:20:22 +0530

    structs

---
+++ go_12_struct/go_1_structs/go_structs_1.go
@@ -0,0 +1,23 @@
+//Structs in go
+
+package main
+
+import "fmt"
+
+type User struct {
+	ID        int
+	firstName string
+	lastName  string
+	email     string
+}
+
+func main() {
+	u := User{
+		ID:        1,
+		firstName: "Krishna",
+		lastName:  "Tandon",
+		email:     "krishnatandon.1208@gmail.com",
+	}
+
+	fmt.Println(u)
+}
